930ab65d1ea9f451ba8b18afc33e97dea8c16c8d
Adhith004 <adhithv446@gmail.com>
2026-09-20 23:59:24 +0530

    Comment for improved readability

--- api/index.py
+++ api/index.py
@@ -1,4 +1,5 @@
-import os #index.py is the Flask backend server that receives PDF uploads and questions, calls the RAG logic in rag.py, and returns AI-generated answers to the user.
+import os #index.py is the Flask backend server that receives PDF uploads and questions,
+#calls the RAG logic in rag.py, and returns AI-generated answers to the user.
 import sys
 import time
 from pathlib import Path
